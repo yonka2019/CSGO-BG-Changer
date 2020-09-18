e82429d3891be4c371f64ac86bbe62ee612a962b
Yonka <yonka2003@gmail.com>
2020-09-19 01:40:39 +0300

    release

---
+++ CSGO_BG_Changer/CSGO Path.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace CSGO_BG_Changer
+{
+    partial class CSGO_Path
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CSGO_Path));
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.ChangePathButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current path:";
+            // 
+            // PathBox
+            // 
+            this.PathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PathBox.Location = new System.Drawing.Point(134, 23);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.ReadOnly = true;
+            this.PathBox.Size = new System.Drawing.Size(330, 24);
+            this.PathBox.TabIndex = 1;
+            // 
+            // ChangePathButton
+            // 
+            this.ChangePathButton.BackColor = System.Drawing.Color.Transparent;
+            this.ChangePathButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChangePathButton.Location = new System.Drawing.Point(182, 62);
+            this.ChangePathButton.Name = "ChangePathButton";
+            this.ChangePathButton.Size = new System.Drawing.Size(126, 37);
+            this.ChangePathButton.TabIndex = 2;
+            this.ChangePathButton.Text = "Change Path";
+            this.ChangePathButton.UseVisualStyleBackColor = false;
+            this.ChangePathButton.Click += new System.EventHandler(this.ChangePathButton_Click);
+            // 
+            // CSGO_Path
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(491, 111);
+            this.Controls.Add(this.ChangePathButton);
+            this.Controls.Add(this.PathBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CSGO_Path";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CS:GO Background Manager";
+            this.Load += new System.EventHandler(this.CSGO_Path_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.Button ChangePathButton;
+    }
+}
